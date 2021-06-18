8b6fc868d384e35eab31dd854a068d1b83fedff3
Kamil Gierach-Pacanek <20944885+KamilPacanek@users.noreply.github.com>
2021-06-18 20:04:17 +0200

    Add spaCy module

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from flask import Flask
+from flask import request, escape
+
+#import en_core_web_trf
+import pl_core_news_lg
+
 
 app = Flask(__name__)
+#nlp_en = en_core_web_trf.load()
+#nlp = {"en" : nlp_en, "pl":nlp_pl}
+nlp_pl = pl_core_news_lg.load()
+nlp = { "pl":nlp_pl}
 
-@app.route("/")
-def index():
-    return "Congratulations, it's a web app!"
+@app.route("/api/ping")
+def ping():
+    return "Pong!"
+
+@app.route("/api/<string:lang>/analyze", methods = ['POST'])
+def analyze(lang):
+    try:
+        text = request.form["s"]
+        doc = nlp[lang](text)
+        print([(w.text, w.pos_) for w in doc])
+        return ""
+    except:
+        return ""
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
